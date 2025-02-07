da693a82bbd53c8089a85709bac3e31d889e4282
smadrigalcef <smadrigal@unicef.org>
2025-02-07 16:14:02 +0100

    feat(components): minimum updates

--- components/CurrentBook.tsx
+++ components/CurrentBook.tsx
@@ -23,42 +23,42 @@ const CurrentBook = () => {
                         </div>
                         <div className="flex gap-5 pb-3">
                             <div className="relative h-[250px] w-[180px] flex-shrink-0">
-                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-contain" />
+                                <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-cover" />
+                            </div>
+                            <div className="">
+                                <div>Leyendo...</div>
+                                <div className="text-2xl font-medium tracking-tight mb-2">Una memoria llamada imperio</div>
+                                <div className="flex justify-between">
+                                    <div className="px-2 py-1 rounded-sm border-2 border-black tracking-tight bg-pink-700 inline-block text-white shadow-neoblack ">Arkady Martine</div>
+                                    <Button className="bg-blue-700 border-black border-2 shadow-neoblack hover:shadow-neoblack-hover  transition-all">Editar</Button>
+                                </div>
+                                <div className="flex gap-2 flex-wrap mt-4">
+                                    {TESTTags.map((tag, index) => (
+                                        <div key={index} className="text-sm py-1 px-2 rounded-sm border border-slate-900 tracking-tight bg-lime-500 text-black">{tag}</div>
+                                    ))}
+                                </div>
+                                <div className="flex gap-4 mt-3">
+                                    <div className="py-1 px-2 rounded-sm border border-slate-900 bg-white shadow-neoblack flex gap-2 items-center">
+                                        <div className="text-xl bg-blue-700 text-white border-black rounded-full p-1 border "><VscPercentage /></div>
+                                        <div className="text-xl tracking-tight">10%</div>
+                                    </div>
+                                    <div className="py-1 px-2 rounded-sm border border-slate-900 bg-white shadow-neoblack flex gap-2 items-center">
+                                        <div className="text-xl bg-orange-700 text-white rounded-full p-1 border border-black "><IoBookOutline /></div>
+                                        <div className="text-xl tracking-tight">300</div>
+                                    </div>
+                                    <div className="py-1 px-2 rounded-sm border border-slate-900 bg-white shadow-neoblack flex gap-2 items-center">
+                                        <div className="text-lg bg-purple-700 text-white rounded-full p-1 border border-black"><MdCalendarToday /></div>
+                                        <div className="text-xl tracking-tight">Ene</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="">
-                            <div>Leyendo...</div>
-                            <div className="text-2xl font-medium tracking-tight mb-2">Una memoria llamada imperio</div>
-                            <div className="flex justify-between">
-                                <div className="px-2 py-1 rounded-sm border-2 border-black tracking-tight bg-pink-700 inline-block text-white shadow-neoblack ">Arkady Martine</div>
-                                <Button className="bg-blue-700 border-black border-2 shadow-neoblack hover:shadow-neoblack-hover  transition-all">Editar</Button>
-                            </div>
-                            <div className="flex gap-2 flex-wrap mt-4">
-                                {TESTTags.map((tag, index) => (
-                                    <div key={index} className="text-sm py-1 px-2 rounded-sm border border-slate-900 tracking-tight bg-lime-500 text-black">{tag}</div>
-                                ))}
-                            </div>
-                            <div className="flex gap-4 mt-3">
-                                <div className="py-1 px-2 rounded-sm border border-slate-900 bg-white shadow-neoblack flex gap-2 items-center">
-                                    <div className="text-xl bg-blue-700 text-white border-black rounded-full p-1 border "><VscPercentage /></div>
-                                    <div className="text-xl tracking-tight">10%</div>
-                                </div>
-                                <div className="py-1 px-2 rounded-sm border border-slate-900 bg-white shadow-neoblack flex gap-2 items-center">
-                                    <div className="text-xl bg-orange-700 text-white rounded-full p-1 border border-black "><IoBookOutline /></div>
-                                    <div className="text-xl tracking-tight">300</div>
-                                </div>
-                                <div className="py-1 px-2 rounded-sm border border-slate-900 bg-white shadow-neoblack flex gap-2 items-center">
-                                    <div className="text-lg bg-purple-700 text-white rounded-full p-1 border border-black"><MdCalendarToday /></div>
-                                    <div className="text-xl tracking-tight">Ene</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex items-center w-full justify-center">
+                        <div className="flex items-center w-full justify-center">
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
         </div >
     )
--- components/NewBooks.tsx
+++ components/NewBooks.tsx
@@ -10,10 +10,15 @@ import Image from "next/image"
 const NewBooks = () => {
     return (
         <div className="container mx-auto p-10 max-w-[1000px] mt-5">
-            <h2 className="mb-12 text-3xl text-center bg-yellow-400 font-semibold inline-block px-3 py-2 border-2 border-black shadow-neoblack">Novedades en la base de datos</h2>
-
+            <div className="flex flex-col mb-5">
+                <div className="-mb-2 z-10 w-fit ml-4 text-sm uppercase bg-pink-400 px-2 py-1 border-2 border-black shadow-neoblack font-semibold rounded">
+                    New
+                </div>
+                <h2 className="text-3xl bg-yellow-400 font-semibold inline-block px-3 py-2 border-2 border-black shadow-neoblack">
+                    Novedades en la biblioteca
+                </h2>
+            </div>
             <Carousel
-
                 opts={{
                     align: "start",
                 }}
@@ -22,27 +27,27 @@ const NewBooks = () => {
                 <CarouselContent>
                     <CarouselItem className="md:basis-1/2 lg:basis-1/4">
                         <div className="relative h-[250px] w-[180px] flex-shrink-0">
-                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-contain" />
+                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-cover" />
                         </div>
                     </CarouselItem>
                     <CarouselItem className="md:basis-1/2 lg:basis-1/4">
                         <div className="relative h-[250px] w-[180px] flex-shrink-0">
-                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-contain" />
+                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-cover" />
                         </div>
                     </CarouselItem>
                     <CarouselItem className="md:basis-1/2 lg:basis-1/4">
                         <div className="relative h-[250px] w-[180px] flex-shrink-0">
-                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-contain" />
+                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-cover" />
                         </div>
                     </CarouselItem>
                     <CarouselItem className="md:basis-1/2 lg:basis-1/4">
                         <div className="relative h-[250px] w-[180px] flex-shrink-0">
-                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-contain" />
+                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-cover" />
                         </div>
                     </CarouselItem>
                     <CarouselItem className="md:basis-1/2 lg:basis-1/4">
                         <div className="relative h-[250px] w-[180px] flex-shrink-0">
-                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-contain" />
+                            <Image src="/images/test.jpg" fill alt="Book" className="border-2 border-black rounded-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] object-cover" />
                         </div>
                     </CarouselItem>
                 </CarouselContent>
